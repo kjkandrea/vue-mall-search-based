cee0b79a48740caaa66222c372bd537798d12871
andrea <kjkandrea@gmail.com>
2021-05-09 17:12:04 +0900

    Add init query

--- src/components/search/SearchController.vue
+++ src/components/search/SearchController.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="onSubmit">
-    <keyword-input :keyword.sync="query.keyword" />
+    <keyword-input :keyword.sync="searchQuery.keyword" />
     <product-filter />
   </form>
 </template>
@@ -10,25 +10,38 @@ import { Vue, Component } from "vue-property-decorator";
 import KeywordInput from "./formFields/KeywordInput.vue";
 import ProductFilter from "./formFields/ProductFilter.vue";
 import { isEqual } from "lodash";
+import { SearchRequest } from "../../types";
+import { Dictionary } from "vue-router/types/router";
 
 @Component({
   components: { ProductFilter, KeywordInput },
 })
 export default class SearchController extends Vue {
-  private query = {
+  private searchQuery: SearchRequest = {
     keyword: "",
   };
+
+  created(): void {
+    this.initQuery();
+  }
+
+  private initQuery(): void {
+    const queryBuffet = this.$route.query;
+    const { keyword } = queryBuffet;
+    if (!keyword) return;
+    this.searchQuery.keyword = keyword.toString();
+  }
 
   private onSubmit() {
     this.pushQuery();
   }
 
   private pushQuery(): void {
-    const equal = isEqual(this.$route.query, this.query);
+    const equal = isEqual(this.$route.query, this.searchQuery);
 
     if (equal) return;
     this.$router.push({
-      query: this.query,
+      query: this.searchQuery as Dictionary<string>,
     });
   }
 }
